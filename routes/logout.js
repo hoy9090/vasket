a1a4b662893cbf3302163e6a31e1a3c2c7b5c5c4
sangkyulee <sklee7753@gmail.com>
2015-10-27 13:43:10 +0900

    logout 추가

--- routes/logout.js
+++ routes/logout.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	req.session.userid = null;
-  res.render('main');
+  res.redirect('main');
 });
 
 module.exports = router;
