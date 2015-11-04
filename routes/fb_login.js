e2536629cf1ac9091bf74b343502cd10973dec9a
sangkyulee <sklee7753@gmail.com>
2015-11-04 20:14:01 +0900

    fb signup

--- routes/fb_login.js
+++ routes/fb_login.js
@@ -32,9 +32,7 @@ router.post('/', function(req, res, next) {
 				else {
 					connection.release();
 					console.log('FIRST TIME!');
-					res.send({id: req.body.id, name: req.body.name, birthday: req.body.birthday, gender: req.body.gender});
-					res.end();
-					res.redirect('/signup');
+					return res.render('signup', {id: req.body.id, name: req.body.name, birthday: req.body.birthday, gender: req.body.gender});
 				}
 			});
 		});
