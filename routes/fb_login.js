eb69d1deeabac6888b9e76b2e4375e6b9b5cc9a5
sangkyulee <sklee7753@gmail.com>
2015-11-04 14:50:58 +0900

    fb first login add

--- routes/fb_login.js
+++ routes/fb_login.js
@@ -1,11 +1,45 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1012'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if (req.query.userid)
-  	req.session.userid = req.query.userid;
-  res.redirect('/');
+	var userid = req.query.userid;
+	if (userid) {
+		pool.getConnection(function(err, connection) {
+			if (err) {
+				console.error('DB Connection error!!');
+				return;
+			}
+			console.log('DB Connection Success!!');
+			connection.query('use vasket');
+			connection.query('select count(*) result from user where userID=?', 
+			[userid], function(err, result, field) {
+				var isAlreadyUser = result[0].result;
+				console.log(isLogin);
+				if (isAlreadyUser == 1) {
+					req.session.userid = userid;
+					res.redirect('/');
+					res.end();
+					console.log('DB FB Login Success!!');
+					//session config
+				}
+				else {
+					res.redirect('/signup?userid='+userid);
+					res.end();
+				}
+				connection.release();
+			});
+		});
+	} else {
+		res.redirect('/');
+		res.end();
+	}
 });
 
 module.exports = router;
