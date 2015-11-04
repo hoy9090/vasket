87ecf6cc74bb2c5acf79b83ea7bd39671d12cdc8
sangkyulee <sklee7753@gmail.com>
2015-11-04 15:03:19 +0900

    fb first login add

--- routes/signup.js
+++ routes/signup.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	var json = {};
-	json[userid] = req.query.userid;
+	json['userid'] = req.query.userid;
   res.render('signup', json);
 });
 
