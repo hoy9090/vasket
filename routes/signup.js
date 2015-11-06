519e0f5b2e9f5c075f8985f1cd28ed42a1e435e1
sangkyulee <sklee7753@gmail.com>
2015-11-06 18:02:34 +0900

    fb signup

--- routes/signup.js
+++ routes/signup.js
@@ -3,10 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('signup');
-});
-
-router.post('/', function(req, res, next) {
   res.render('signup', req.body);
 });
 
