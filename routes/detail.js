2131fd908308047dd52a19d56a75d66c5552ebd0
sangkyulee <sklee7753@gmail.com>
2015-11-11 18:19:08 +0900

    detail

--- routes/detail.js
+++ routes/detail.js
@@ -1,19 +1,53 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1012'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	if (!req.session.userid)
 		res.render('main');
-	else
-  	res.render('detail');
+	else {
+		pool.getConnection(function(err, connection) {
+			connection.query('select (select email from user where user.userno=comment.userno) email, content from comment order by commentNo desc;',
+				function(err, result, field) {
+				if (err) {
+					console.error('DB Selection error!!');
+					return;
+				}
+				for (var key in result) {
+					console.log(key.email, key.content);
+				}
+				connection.release();
+				res.render('detail', result);
+			});
+		});
+  	}
 });
 
 router.post('/', function(req, res, next) {
   if (!req.session.userid)
 		res.render('main');
-	else
-  	res.render('detail');
+	else {
+		pool.getConnection(function(err, connection) {
+			connection.query('select (select email from user where user.userno=comment.userno) email, content from comment order by commentNo desc;',
+				function(err, result, field) {
+				if (err) {
+					console.error('DB Selection error!!');
+					return;
+				}
+				for (var key in result) {
+					console.log(key.email, key.content);
+				}
+				connection.release();
+				res.render('detail', result);
+			});
+		});
+  	}
 });
 
 module.exports = router;
