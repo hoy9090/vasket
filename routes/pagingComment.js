fa597c804023258471e0e132c81de0f7b79978eb
sangkyulee <sklee7753@gmail.com>
2015-11-13 12:59:44 +0900

    detail

--- routes/pagingComment.js
+++ routes/pagingComment.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var pageNo = res.query.pageNo;
+  var pageNo = req.query.pageNo;
   res.redirect(200, 'detail#secC?pageNo='+pageNo);
 });
 
