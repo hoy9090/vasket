26a24ebaad9bc1253c7f4b14172b277ad911c891
sangkyulee <sklee7753@gmail.com>
2015-11-15 18:37:16 +0900

    payment

--- routes/logout.js
+++ routes/logout.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	req.session = null;
+	req.session.userid = null;
+	req.session.email = null;
+	req.session.name = null;
+	req.session.userNo = null;
   res.redirect('/');
 });
 
