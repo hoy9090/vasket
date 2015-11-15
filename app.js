16f6aa6331e735b949d07595c95325ce3a7824b5
sangkyulee <sklee7753@gmail.com>
2015-11-15 14:24:30 +0900

    detail

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ var mypage = require('./routes/mypage');
 var detail = require('./routes/detail');
 var payment = require('./routes/payment');
 var reg_comment = require('./routes/reg_comment');
+var customer = require('./routes/customer');
 
 var app = express();
 
@@ -56,6 +57,7 @@ app.use('/mypage', mypage);
 app.use('/detail', detail);
 app.use('/payment', payment);
 app.use('/reg_comment', reg_comment);
+app.use('/customer', customer);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
