2dbe6eaf53905294e3287bec2ecf7bb7b827cba5
sangkyulee <sklee7753@gmail.com>
2015-11-17 17:16:43 +0900

    payment

--- routes/pay_finish.js
+++ routes/pay_finish.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  console.log(req.body.name, req.body.phone, req.body.addr);
+  if (req.body.tab == '#secA')
+  	console.log(req.body.name, req.body.phone, req.body.addr);
+  else {
+  	console.log(req.body.name, req.body.phone, req.body.addr, req.body.add, req.body.isDefault);
+  }
   res.end();
 });
 
