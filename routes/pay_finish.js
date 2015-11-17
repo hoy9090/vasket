42d0d40465693b82ee26f815b884ad3cbf8cc310
sangkyulee <sklee7753@gmail.com>
2015-11-17 16:51:55 +0900

    payment

--- routes/pay_finish.js
+++ routes/pay_finish.js
@@ -4,6 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.post('/', function(req, res, next) {
   console.log(req.body.name, req.body.phone, req.body.addr);
+  res.end();
 });
 
 module.exports = router;
