7eff416c3ce7ddff7c8c6007a975615f7832cd03
sangkyulee <sklee7753@gmail.com>
2015-11-21 22:07:26 +0900

    overall

--- routes/service.js
+++ routes/service.js
@@ -9,29 +9,33 @@ var pool = mysql.createPool({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	pool.getConnection(function(err, connection) {
-		if (err) {
-			console.error('DB Connection error!!');
-			return;
-		}
-		console.log('DB Service Connection Success!!');
-		connection.query('use vasket');
-		connection.query('select brandNo, brandName, brandContent from brand', function(err, result, field) {
+	if (req.session.userNo) {
+		pool.getConnection(function(err, connection) {
 			if (err) {
-				console.error(err);
+				console.error('DB Connection error!!');
 				return;
 			}
-			var brand = result;
-			connection.query('select brandNo from brandLikeList where userNo=?', [req.session.userNo], function(err, result, field) {
+			console.log('DB Service Connection Success!!');
+			connection.query('use vasket');
+			connection.query('select brandNo, brandName, brandContent from brand', function(err, result, field) {
 				if (err) {
 					console.error(err);
 					return;
 				}
-				var brandLike = result;
-				res.render('service', {brand: brand, brandLike: brandLike});	
+				var brand = result;
+				connection.query('select brandNo from brandLikeList where userNo=?', [req.session.userNo], function(err, result, field) {
+					if (err) {
+						console.error(err);
+						return;
+					}
+					var brandLike = result;
+					res.render('service', {brand: brand, brandLike: brandLike});	
+				});
 			});
 		});
-	});
+	} else {
+		res.render('main');
+	}
 });
 
 module.exports = router;
