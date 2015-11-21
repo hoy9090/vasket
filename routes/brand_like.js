b5ab70065a5f871fd12fc76c192dd77faf612979
sangkyulee <sklee7753@gmail.com>
2015-11-21 21:53:33 +0900

    overall

--- routes/brand_like.js
+++ routes/brand_like.js
@@ -17,11 +17,18 @@ router.post('/', function(req, res, next) {
 		console.log('DB Service Connection Success!!');
 		connection.query('use vasket');
 		if (req.body.command == 'insert') {
-			connection.query('insert into brandLikeList(brandNo, userNo) values(?, ?)', [req.body.brandNo, req.session.userNo]);
+			connection.query('insert into brandLikeList(brandNo, userNo) values(?, ?)', [req.body.brandNo, req.session.userNo], function(err, result, field) {
+				if (err)
+					console.error(err);
+			});
 		} else if (req.body.command == 'delete') {
-			connection.query('delete from brandLikeList where userNo=? and brandNo=?', [req.session.userNo, req.body.brandNo]);
+			connection.query('delete from brandLikeList where userNo=? and brandNo=?', [req.session.userNo, req.body.brandNo], function(err, result, field) {
+				if (err)
+					console.error(err);
+			});
 		}
 		console.log('LIKE change success!!');
+		connection.release();
 	});
 });
 
