0abf336a24a28c6e35687dbcbc66a995259cb0b4
sangkyulee <sklee7753@gmail.com>
2015-11-22 21:18:11 +0900

    overall

--- app.js
+++ app.js
@@ -32,8 +32,8 @@ var inquiry = require('./routes/inquiry');
 var app = express();
 
 var options = {
-  key: fs.readFileSync('./ssl/key.pem'),
-  cert: fs.readFileSync('./ssl/crt.pem')
+  key: fs.readFileSync(__dirname + '/ssl/key.pem'),
+  cert: fs.readFileSync(__dirname + '/ssl/crt.pem')
 };
 
 // view engine setup
