3a5733618d9700315d5c91c6445a5077e2d75284
sangkyulee <sklee7753@gmail.com>
2015-11-26 20:06:45 +0900

    inquiry_detail

--- routes/mypage.js
+++ routes/mypage.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1012'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	if (!req.session.userid)
 		res.redirect('/');
-	else
-  		res.render('mypage', {email: req.session.email});
+	else {
+		pool.getConnection(function(err, connection) {
+			connection.query('use vasket');
+			connection.query('select brandName brand, brandContent content from brand inner join (select brandNo from brandlike where userNo='+req.session.userNo+') brandlike where brandlike.brandNo=brand.brandNo',
+				function(err, result, field) {
+				if (err) {
+					console.error('DB inquiry Selection error!!');
+					return;
+				}
+				connection.release();
+				res.render('mypage', {email: req.session.email, result: result});
+			});
+		});
+	}
 });
 
 module.exports = router;
