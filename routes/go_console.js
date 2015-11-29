231a3d6f1d98d4fe83c540cbb478e11c72be45ac
sangkyulee <sklee7753@gmail.com>
2015-11-29 16:56:43 +0900

    admin

--- routes/go_console.js
+++ routes/go_console.js
@@ -6,7 +6,7 @@ router.post('/', function(req, res, next) {
 	if (req.body.id == 'vasket_admin' && req.body.pw == 'vasket20151012')
   		res.render('main');
   	else
-  		res.end();
+  		res.redirect('main');
 });
 
 module.exports = router;
