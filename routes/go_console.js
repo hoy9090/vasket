8e4aba501c1f1fdb305cf22ac9b2e51a8c0c1eb2
sangkyulee <sklee7753@gmail.com>
2015-11-29 17:09:03 +0900

    admin

--- routes/go_console.js
+++ routes/go_console.js
@@ -3,9 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-	if (req.body.id == 'vasket_admin' && req.body.pw == 'vasket20151012')
+	if (req.body.id == 'vasket_admin' && req.body.pw == 'vasket20151012') {
+		req.url = 'http://vasket.co.kr/console';
   		res.render('main');
-  	else
+	} else
   		res.redirect('main');
 });
 
