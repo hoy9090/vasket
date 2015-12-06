dd2c1702b928ff546b8d08b1cc3c3372e4d1daf3
sangkyulee <sklee7753@gmail.com>
2015-12-06 18:29:40 +0900

    contact

--- app.js
+++ app.js
@@ -53,9 +53,8 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser({uploadDir: path.join(__dirname, '/tmp')}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({uploadDir: path.join(__dirname, '/tmp'), extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
