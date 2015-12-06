d0c1e976988d962a70c808c8ddd6c99335a98c43
sangkyulee <sklee7753@gmail.com>
2015-12-06 21:21:17 +0900

    admin

---
+++ routes/product_form.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	if (req.header('Referer').endsWith('admin_console'))
+		res.render('product_form');
+	else
+		res.end();
+});
+
+module.exports = router;
