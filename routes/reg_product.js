27f5366fdcd3c7133620ea80c4d411c5fa886501
sangkyulee <sklee7753@gmail.com>
2015-12-06 21:51:20 +0900

    admin

---
+++ routes/reg_product.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1012'
+});
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+	pool.getConnection(function(err, connection) {
+		connection.query('use vasket');
+		connection.query('insert into productlist(brandNo, productPrice, productName, productComment, productState, productInven, uploadDate) values(?, ?, ?, ?, "판매중", ?, sysdate())', [req.body.brand, req.body.price, req.body.name, req.body.content, req.body.inven],
+			function(err, result, field) {
+				if (err) {
+					console.error(err);
+					return;
+				}
+				connection.release();
+				res.send({id: 'vasket', pw: '20151012'});
+				res.redirect('/admin_console');
+		});
+	});
+});
+
+module.exports = router;
