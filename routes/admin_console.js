18fd34955c8dbaeff0863dca88057d0b7bc7dd5c
sangkyulee <sklee7753@gmail.com>
2015-12-06 21:59:39 +0900

    admin

--- routes/admin_console.js
+++ routes/admin_console.js
@@ -43,4 +43,39 @@ router.post('/', function(req, res, next) {
   		res.redirect('main');
 });
 
+router.get('/', function(req, res, next) {
+	if (req.header('Referer') && req.header('Referer').endsWith('product_form'))
+		pool.getConnection(function(err, connection) {
+				connection.query('use vasket');
+				connection.query('select userName name, userID id, joindate date, email from user',
+					function(err, result, field) {
+						if (err) {
+							console.error(err);
+							return;
+						}
+						var user = result;
+						connection.query('select productName name, productNo no, brand.brandName brand, productPrice price, productInven inven, productState state, productComment comment from productlist left outer join brand on (productlist.brandNo=brand.brandNo)',
+							function(err, result, field) {
+								if (err) {
+									console.error(err);
+									return;
+								}
+								var product = result;
+								connection.query('select partnerName name, partnerType type, partnerAddr addr, repName rep_name, repPhone rep_phone, repEmail rep_email, file from partner',
+									function(err, result, field) {
+										if (err) {
+											console.error(err);
+											return;
+										}
+										var partner = result;
+										connection.release();
+										res.render('admin_console', {user: user, product: product, partner: partner});
+								});
+					});
+				});
+		});
+	else
+		res.end();
+});
+
 module.exports = router;
