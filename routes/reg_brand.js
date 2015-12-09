0d11fbbdada663a08f99ffad98f45fa62658c400
sangkyulee <sklee7753@gmail.com>
2015-12-09 20:49:44 +0900

    brand

--- routes/reg_brand.js
+++ routes/reg_brand.js
@@ -13,15 +13,18 @@ var upload = multer({dest: 'public/images/brand_logo/'});
 
 /* GET home page. */
 router.post('/', upload.single('file'), function(req, res, next) {
+	console.log(req.file);
 	pool.getConnection(function(err, connection) {
 		connection.query('use vasket');
 		connection.query('insert into brand(brandName, brandNation, brandContent, brandCategory, brandImageName) values(?, ?, ?, ?, ?)', [req.body.name, req.body.nation, req.body.content, req.body.type, req.body.filename],
 			function(err, result, field) {
+				console.log(req.file);
 				if (err) {
 					console.error(err);
 					return;
 				}
 				connection.release();
+				console.log(req.file);
 				fs.rename('public/images/brand_logo/'+req.file.name, 'public/images/brand_logo/'+req.body.filename);
 				res.redirect('/admin_console');
 		});
