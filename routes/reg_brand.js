cf82d755c7f2978f32afe48c63abe23d76a611d9
sangkyulee <sklee7753@gmail.com>
2015-12-09 20:09:00 +0900

    brand

--- routes/reg_brand.js
+++ routes/reg_brand.js
@@ -8,13 +8,12 @@ var pool = mysql.createPool({
 });
 var path = require('path');
 var multer = require('multer');
-var upload = multer({dest: 'public/images/brand_logo/', rename: function(fieldname, filename, req, res) {
+
+router.use(multer({dest: 'public/images/brand_logo/', rename: function(fieldname, filename, req, res) {
 							console.log(req.body.filename);
 					       return req.body.filename;
 					}
-			});
-
-router.use(upload);
+			}));
 
 /* GET home page. */
 router.post('/', upload.single('file'), function(req, res, next) {
