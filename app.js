96376324a56cfeb4dfb83610099494e3111a5665
sangkyulee <sklee7753@gmail.com>
2015-12-09 17:41:19 +0900

    service

--- app.js
+++ app.js
@@ -53,6 +53,7 @@ app.set('port1', 80);
 app.set('port2', 443);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('env', 'development');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
