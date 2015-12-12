df3bc629c1319fa42445cb037c5bf482af774ca3
sangkyulee <sklee7753@gmail.com>
2015-12-12 19:37:10 +0900

    brand

---
+++ routes/add_goods.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1012'
+});
+
+/* GET home page. */
+router.get('/:productNo', function(req, res, next) {
+	if (req.header("Referer") && req.header("Referer").contains("/detail")) {
+		var basket = req.session.basket;
+		if (!basket)
+			basket = [];
+		basket.push({"no": req.params.productNo, "count": 1, "sns": 0, "as": 0, "return": 0});
+		res.redirect('/basket');
+	} else {
+		res.redirect('back');
+	}
+});
+
+module.exports = router;
--- routes/basket.js
+++ routes/basket.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	if (req.session.userid)
-  		res.render('basket');
+	if (req.session.userid) {
+		var basket = req.session.basket;
+		if (!basket)
+			basket = [];
+  		res.render('basket', {basket: basket});
+	}
   	else
   		res.redirect('/');
 });
