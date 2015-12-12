a5504badca82b8a3627db7b2b6c2906066ad6c6d
sangkyulee <sklee7753@gmail.com>
2015-12-12 20:51:02 +0900

    brand

--- routes/add_goods.js
+++ routes/add_goods.js
@@ -12,9 +12,15 @@ String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
 /* GET home page. */
 router.get('/:productNo', function(req, res, next) {
 	if (req.header("Referer") && req.header("Referer").contains("/detail")) {
+		var productNo = parseInt(req.params.productNo);
 		if (!req.session.basket)
 			req.session.basket = [];
-		req.session.basket.push({"no": parseInt(req.params.productNo), "count": 1, "sns": 0, "as": 0, "return": 0});
+		for (var i = 0 ; i < req.session.basket.length ; i++) {
+			if (req.session.basket[i].no == productNo)
+				break;
+			if (i == req.session.basket.length-1)
+				req.session.basket.push({"no": productNo, "count": 1, "sns": 0, "as": 0, "return": 0});
+		}
 		console.log(req.session.basket);
 		res.redirect('/basket');
 	} else {
