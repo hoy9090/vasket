1395fa274ec42eda757e5ded485daaec56c40e07
sangkyulee <sklee7753@gmail.com>
2015-12-12 16:55:59 +0900

    brand

--- routes/community_post.js
+++ routes/community_post.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1012'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('community_post');
+	pool.getConnection(function(err, connection) {
+		if (err) {
+			console.error('DB Connection error!!');
+			return;
+		}
+		connection.query('use vasket');
+		connection.query('select brandName brand from brand', function(err, result, field) {
+			connection.release();
+			res.render('community_post', {brand: result});
+		});
+	});
 });
 
 module.exports = router;
