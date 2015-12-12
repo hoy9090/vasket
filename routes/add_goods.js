47d12d99b4377810d09d74e4776e064e8b2fc1db
sangkyulee <sklee7753@gmail.com>
2015-12-12 20:52:18 +0900

    brand

--- routes/add_goods.js
+++ routes/add_goods.js
@@ -13,14 +13,16 @@ String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
 router.get('/:productNo', function(req, res, next) {
 	if (req.header("Referer") && req.header("Referer").contains("/detail")) {
 		var productNo = parseInt(req.params.productNo);
-		if (!req.session.basket)
+		if (!req.session.basket) {
 			req.session.basket = [];
-		for (var i = 0 ; i < req.session.basket.length ; i++) {
-			if (req.session.basket[i].no == productNo)
-				break;
-			if (i == req.session.basket.length-1)
-				req.session.basket.push({"no": productNo, "count": 1, "sns": 0, "as": 0, "return": 0});
-		}
+			req.session.basket.push({"no": productNo, "count": 1, "sns": 0, "as": 0, "return": 0});
+		} else 
+			for (var i = 0 ; i < req.session.basket.length ; i++) {
+				if (req.session.basket[i].no == productNo)
+					break;
+				if (i == req.session.basket.length-1)
+					req.session.basket.push({"no": productNo, "count": 1, "sns": 0, "as": 0, "return": 0});
+			}
 		console.log(req.session.basket);
 		res.redirect('/basket');
 	} else {
