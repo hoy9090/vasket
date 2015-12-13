554eccd51a20a79cfb78525026a5294f74f5b835
sangkyulee <sklee7753@gmail.com>
2015-12-13 18:29:48 +0900

    basket

--- routes/basket.js
+++ routes/basket.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1012'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +13,17 @@ router.get('/', function(req, res, next) {
 		var basket = req.session.basket;
 		if (!basket)
 			basket = [];
-  		res.render('basket', {basket: basket});
+		var queryString = "";
+		for (var index in basket) {
+			queryString += basket[index]+",";
+		}
+		queryString = queryString.substring(0, queryString.length()-1);
+		pool.getConnection(function(err, connection) {
+			connection.query('use vasket');
+			connection.query('select productName name, productComment comment, productPrice price from productlist where productNo in ('+queryString+') order by find_in_set(productNo, `'+queryString+'`)', function(err, result, field) {
+				res.render('basket', {basket: basket, info: result});	
+			});
+	  	});
 	}
   	else
   		res.redirect('/');
