32289970781ca0cb2c32f66480d54c95242679bc
sangkyulee <sklee7753@gmail.com>
2015-12-20 03:29:53 +0900

    aa

--- routes/service.js
+++ routes/service.js
@@ -35,7 +35,7 @@ router.get('/', function(req, res, next) {
 							return;
 						}
 						var product = result;
-						connection.query('select community.communityNo, email, brandName, brandImageName, image, view, community.content, (select count(commentNo) from community, communityComment where community.communityNo = communityComment.commentNo) as count from community join user on community.userNo = user.userNo join brand on community.brandNo = brand.brandNo order by communityNo desc', function(err, result, field) {
+						connection.query('select community.communityNo, email, brandName, brandImageName, image, view, community.content, (select count(commentNo) from community, communityComment where community.communityNo = communityComment.commentNo) as count, (select count(*) from communityLikeList, community where communityLikeList.communityNo = community.communityNo) as `like` from community join user on community.userNo = user.userNo join brand on community.brandNo = brand.brandNo order by communityNo desc', function(err, result, field) {
 							if (err) {
 								console.error(err);
 							}
@@ -70,7 +70,7 @@ router.get('/', function(req, res, next) {
 						return;
 					}
 					var product = result;
-					connection.query('select community.communityNo, email, brandName, brandImageName, image, view, community.content, (select count(commentNo) from community, communityComment where community.communityNo = communityComment.commentNo) as count from community join user on community.userNo = user.userNo join brand on community.brandNo = brand.brandNo order by communityNo desc', function(err, result, field) {
+					connection.query('select community.communityNo, email, brandName, brandImageName, image, view, community.content, (select count(commentNo) from community, communityComment where community.communityNo = communityComment.commentNo) as count, (select count(*) from communityLikeList, community where communityLikeList.communityNo = community.communityNo) as `like` from community join user on community.userNo = user.userNo join brand on community.brandNo = brand.brandNo order by communityNo desc', function(err, result, field) {
 						if (err) {
 							console.error(err);
 						}
