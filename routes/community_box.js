a11c192287e34d88608642ddf5ce12c5d35afeb2
sangkyulee <sklee7753@gmail.com>
2015-12-20 05:14:08 +0900

    aa

--- routes/community_box.js
+++ routes/community_box.js
@@ -10,20 +10,35 @@ var pool = mysql.createPool({
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	var communityNo = req.query.communityNo;
-	if (communityNo)
-		pool.getConnection(function(err, connection) {
-			if (err)
-				console.error(err);
-			connection.query('use vasket');
-			connection.query('update community set view=view+1 where communityNo='+communityNo);
-			connection.query('select brandImageName brandImage, brandName, email, image, community.content content, view, (select count(commentNo) from community, communityComment where community.communityNo = communityComment.commentNo) as count, (select count(*) from communityLikeList, community where communityLikeList.communityNo = community.communityNo) as `like` from community join user on community.userNo=user.userNo join brand on community.brandNo=brand.brandNo where communityNo='+communityNo, function(err, result, field) {
-				connection.release();
-				var at_index = result[0].email.indexOf('@');
-				result[0].email = result[0].email.substr(0, 2)+'***@'+result[0].email.substr(at_index+1, 2);
-				res.render('community_box', {community: result[0]});
+	if (communityNo) {
+		if (req.session.userid) {
+			pool.getConnection(function(err, connection) {
+				if (err)
+					console.error(err);
+				connection.query('use vasket');
+				connection.query('update community set view=view+1 where communityNo='+communityNo);
+				connection.query('select brandImageName brandImage, brandName, email, image, community.content content, view, (select count(commentNo) from community, communityComment where community.communityNo = communityComment.commentNo) as count, (select count(*) from communityLikeList, community where communityLikeList.communityNo = community.communityNo) as `like`, (select count(*) from communityLikeList where userNo=? and communityNo=?) user_like from community join user on community.userNo=user.userNo join brand on community.brandNo=brand.brandNo where communityNo='+communityNo, [req.session.userNo, communityNo] ,function(err, result, field) {
+					connection.release();
+					var at_index = result[0].email.indexOf('@');
+					result[0].email = result[0].email.substr(0, 2)+'***@'+result[0].email.substr(at_index+1, 2);
+					res.render('community_box', {community: result[0], communityNo: communityNo});
+				});
 			});
-		});
-	else
+		} else {
+			pool.getConnection(function(err, connection) {
+				if (err)
+					console.error(err);
+				connection.query('use vasket');
+				connection.query('update community set view=view+1 where communityNo='+communityNo);
+				connection.query('select brandImageName brandImage, brandName, email, image, community.content content, view, (select count(commentNo) from community, communityComment where community.communityNo = communityComment.commentNo) as count, (select count(*) from communityLikeList, community where communityLikeList.communityNo = community.communityNo) as `like` from community join user on community.userNo=user.userNo join brand on community.brandNo=brand.brandNo where communityNo='+communityNo, function(err, result, field) {
+					connection.release();
+					var at_index = result[0].email.indexOf('@');
+					result[0].email = result[0].email.substr(0, 2)+'***@'+result[0].email.substr(at_index+1, 2);
+					res.render('community_box', {community: result[0], communityNo: communityNo});
+				});
+			});
+		}
+	} else
 		res.redirect('/');
 });
 
---
+++ routes/community_like.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1012'
+});
+
+/* GET home page. */
+router.post('/', function(req, res, next) {
+	pool.getConnection(function(err, connection) {
+		if (err) {
+			console.error('DB Connection error!!');
+			return;
+		}
+		console.log('DB Service Connection Success!!');
+		connection.query('use vasket');
+		if (req.body.command == 'insert') {
+			connection.query('insert into communityLikeList(communityNo, userNo) values(?, ?)', [req.body.communityNo, req.session.userNo], function(err, result, field) {
+				if (err)
+					console.error(err);
+				connection.query('select count(*) as `count` from communityLikeList where communityNo='+req.body.communityNo, function (err, result, field) {
+					res.send({count: result[0].count});
+					console.log('LIKE change success!!');
+					connection.release();
+				});
+			});
+		} else if (req.body.command == 'delete') {
+			connection.query('delete from communityLikeList where userNo=? and communityNo=?', [req.session.userNo, req.body.communityNo], function(err, result, field) {
+				if (err)
+					console.error(err);
+				connection.query('select count(*) as `count` from communityLikeList where communityNo='+req.body.communityNo, function (err, result, field) {
+					res.send({count: result[0].count});
+					console.log('LIKE change success!!');
+					connection.release();
+				});
+			});
+		} else {
+			connection.release();
+		}
+	});
+});
+
+module.exports = router;
