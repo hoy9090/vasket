536a03ab8d80f0b39bb5cee8edfbccbfee7ab001
sangkyulee <sklee7753@gmail.com>
2015-12-20 03:10:53 +0900

    aa

--- routes/service.js
+++ routes/service.js
@@ -35,7 +35,7 @@ router.get('/', function(req, res, next) {
 							return;
 						}
 						var product = result;
-						connection.query('select communityNo, email, brandName, brandImageName, image, view, community.content, count(commentNo) count from community join user on community.userNo = user.userNo join brand on community.brandNo = brand.brandNo join communityComment on community.communityNo = communityComment.communityNo order by communityNo desc', function(err, result, field) {
+						connection.query('select community.communityNo, email, brandName, brandImageName, image, view, community.content, count(commentNo) count from community join user on community.userNo = user.userNo join brand on community.brandNo = brand.brandNo join communityComment on community.communityNo = communityComment.communityNo order by communityNo desc', function(err, result, field) {
 							if (err) {
 								console.error(err);
 								return;
@@ -71,7 +71,7 @@ router.get('/', function(req, res, next) {
 						return;
 					}
 					var product = result;
-					connection.query('select communityNo, email, brandName, brandImageName, image, view, community.content, count(commentNo) count from community join user on community.userNo = user.userNo join brand on community.brandNo = brand.brandNo join communityComment on community.communityNo = communityComment.communityNo order by communityNo desc', function(err, result, field) {
+					connection.query('select community.communityNo, email, brandName, brandImageName, image, view, community.content, count(commentNo) count from community join user on community.userNo = user.userNo join brand on community.brandNo = brand.brandNo join communityComment on community.communityNo = communityComment.communityNo order by communityNo desc', function(err, result, field) {
 						if (err) {
 							console.error(err);
 							return;
