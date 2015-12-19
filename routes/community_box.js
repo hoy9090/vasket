b1dbbde8706e24788a0824076144f8edff6aac61
sangkyulee <sklee7753@gmail.com>
2015-12-20 02:34:28 +0900

    aa

--- routes/community_box.js
+++ routes/community_box.js
@@ -10,17 +10,21 @@ var pool = mysql.createPool({
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	var communityNo = req.query.communityNo;
-	pool.getConnection(function(err, connection) {
-		if (err)
-			console.error(err);
-		connection.query('use vasket');
-		connection.query('select brandImageName brandImage, brandName, email, image, community.content content, view from community join user on community.userNo=user.userNo join brand on community.brandNo=brand.brandNo where communityNo='+communityNo, function(err, result, field) {
-			connection.release();
-			var at_index = result[0].email.indexOf('@');
-			result[0].email = result[0].email.substr(0, 2)+'***@'+result[0].email.substr(at_index+1, 2);
-			res.render('community_box', {community: result[0]});
+	if (communityNo)
+		pool.getConnection(function(err, connection) {
+			if (err)
+				console.error(err);
+			connection.query('use vasket');
+			connection.query('update community set view=view+1 where communityNo='+communityNo);
+			connection.query('select brandImageName brandImage, brandName, email, image, community.content content, view from community join user on community.userNo=user.userNo join brand on community.brandNo=brand.brandNo where communityNo='+communityNo, function(err, result, field) {
+				connection.release();
+				var at_index = result[0].email.indexOf('@');
+				result[0].email = result[0].email.substr(0, 2)+'***@'+result[0].email.substr(at_index+1, 2);
+				res.render('community_box', {community: result[0]});
+			});
 		});
-	});
+	else
+		res.redirect('/');
 });
 
 module.exports = router;
