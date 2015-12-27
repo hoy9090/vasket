f6f0021036edf31ec92b87f4ce996e295b003e0f
sangkyulee <sklee7753@gmail.com>
2015-12-28 03:29:49 +0900

    db

--- routes/community_box.js
+++ routes/community_box.js
@@ -18,10 +18,17 @@ router.get('/', function(req, res, next) {
 				connection.query('use vasket');
 				connection.query('update community set view=view+1 where communityNo='+communityNo);
 				connection.query('select brandImageName brandImage, brandName, email, image, community.content content, view, (select count(*) from communityComment where communityNo=?) as count, (select count(*) from communityLikeList where communityNo=?) as `like`, (select count(*) from communityLikeList where userNo=? and communityNo=?) user_like from community join user on community.userNo=user.userNo join brand on community.brandNo=brand.brandNo where communityNo='+communityNo, [communityNo, communityNo, req.session.userNo, communityNo] ,function(err, result, field) {
-					connection.release();
-					var at_index = result[0].email.indexOf('@');
-					result[0].email = result[0].email.substr(0, 2)+'***@'+result[0].email.substr(at_index+1, 2);
-					res.render('community_box', {community: result[0], communityNo: communityNo});
+					var community = result[0];
+					var at_index = community.email.indexOf('@');
+					community.email = community.email.substr(0, 2)+'***@'+community.email.substr(at_index+1, 2);
+					connection.query('SELECT email, content FROM communityComment c join user u on c.userNo = u.userNo where communityNo='+communityNo, function(err, result, field) {
+						connection.release();
+						for (var index in result) {
+							var at_index_ = result[index].email.indexOf('@');
+							result[index].email = result[index].email.substr(0, 2)+'***@'+result[index].email.substr(at_index_+1, 2);
+						}
+						res.render('community_box', {community: community, communityNo: communityNo, comment: result});
+					});
 				});
 			});
 		} else {
@@ -31,10 +38,17 @@ router.get('/', function(req, res, next) {
 				connection.query('use vasket');
 				connection.query('update community set view=view+1 where communityNo='+communityNo);
 				connection.query('select brandImageName brandImage, brandName, email, image, community.content content, view, (select count(*) from communityComment where communityNo=?) as count, (select count(*) from communityLikeList where communityNo=?) as `like` from community join user on community.userNo=user.userNo join brand on community.brandNo=brand.brandNo where communityNo='+communityNo, [communityNo, communityNo], function(err, result, field) {
-					connection.release();
-					var at_index = result[0].email.indexOf('@');
-					result[0].email = result[0].email.substr(0, 2)+'***@'+result[0].email.substr(at_index+1, 2);
-					res.render('community_box', {community: result[0], communityNo: communityNo});
+					var community = result[0];
+					var at_index = community.email.indexOf('@');
+					community.email = community.email.substr(0, 2)+'***@'+community.email.substr(at_index+1, 2);
+					connection.query('SELECT email, content FROM communityComment c join user u on c.userNo = u.userNo where communityNo='+communityNo, function(err, result, field) {
+						connection.release();
+						for (var index in result) {
+							var at_index_ = result[index].email.indexOf('@');
+							result[index].email = result[index].email.substr(0, 2)+'***@'+result[index].email.substr(at_index_+1, 2);
+						}
+						res.render('community_box', {community: community, communityNo: communityNo, comment: result});
+					});
 				});
 			});
 		}
