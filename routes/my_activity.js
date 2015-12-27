f61870df5736253054f3751460dcf2de00d187fe
sangkyulee <sklee7753@gmail.com>
2015-12-28 04:58:37 +0900

    db

--- routes/my_activity.js
+++ routes/my_activity.js
@@ -1,8 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var pool = mysql.createPool({
+	host: 'localhost',
+	user: 'root',
+	password: '1012'
+});
 
 router.get('/', function(req, res, next) {
-	res.render('my_activity');
+	if (req.session.userid) {
+		pool.getConnection(function(err, connection) {
+			connection.query('use vasket');
+			connection.query('select c.communityNo, brandImageName, brandName, image, c.content content, view, (select count(*) from communityComment where communityComment.communityNo = c.communityNo) as `count`, (select count(*) from communityLikeList where communityLikeList.communityNo = c.communityNo) as `like`, (select count(*) from communityLikeList where userNo=? and communityNo = c.communityNo) user_like from community c join user ON c.userNo = user.userNo join brand ON c.brandNo = brand.brandNo where c.userNo = ? order by c.communityNo desc;', [req.session.userNo, req.session.userNo], function(err, result, field) {
+				connection.release();
+				res.render('my_activity', {community: result});
+			});
+		});
+
+	} else {
+		res.redirect('back');
+	}
 });
 
 module.exports = router;
