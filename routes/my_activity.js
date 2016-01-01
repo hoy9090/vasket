b72dab606b9a5291959bfb3ef1c35c4aa97550c5
sangkyulee <sklee7753@gmail.com>
2016-01-01 21:41:34 +0900

    aa

--- routes/my_activity.js
+++ routes/my_activity.js
@@ -12,8 +12,17 @@ router.get('/', function(req, res, next) {
 		pool.getConnection(function(err, connection) {
 			connection.query('use vasket');
 			connection.query('select c.communityNo, brandImageName, brandName, image, c.content content, view, (select count(*) from communityComment where communityComment.communityNo = c.communityNo) as `count`, (select count(*) from communityLikeList where communityLikeList.communityNo = c.communityNo) as `like`, (select count(*) from communityLikeList where userNo=? and communityNo = c.communityNo) user_like from community c join user ON c.userNo = user.userNo join brand ON c.brandNo = brand.brandNo where c.userNo = ? order by c.communityNo desc;', [req.session.userNo, req.session.userNo], function(err, result, field) {
-				connection.release();
-				res.render('my_activity', {community: result});
+				if (err) {
+					console.error(err);
+				}
+				var community = result;
+				//connection.query('FILL SQL!!!', function(err, result, field) {
+				//	if (err) {
+				//		console.error(err);
+				//	}
+					connection.release();
+					res.render('my_activity', {community: community});
+				//});
 			});
 		});
 
