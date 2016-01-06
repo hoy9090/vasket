9aba9f305d142963bed8ea3210942152b0a5b122
sangkyulee <sklee7753@gmail.com>
2016-01-06 19:43:47 +0900

    a

--- routes/product_box.js
+++ routes/product_box.js
@@ -18,14 +18,21 @@ router.get('/', function(req, res, next) {
 				connection.query('use vasket');
 				connection.query('update product set view=view+1 where productNo='+productNo);
 				connection.query('select image, imageCount, content, view, category, productName, nation, size, material, description, waterproof, gender, price, code, (select count(*) from productComment where productNo=?) as count, (select count(*) from productLikeList where productNo=?) as `like`, (select count(*) from productClipList where productNo=?) as `clip`, (select count(*) from productClipList where userNo=? and productNo=?) user_clip, (select count(*) from productLikeList where userNo=? and productNo=?) user_like from product where productNo='+productNo, [productNo, productNo, productNo, req.session.userNo, productNo, req.session.userNo, productNo] ,function(err, result, field) {
+					if (err)
+						console.error(err);
 					var product = result[0];
-					connection.query('SELECT email, content FROM productComment c join user u on c.userNo = u.userNo where productNo='+productNo+' order by c.commentNo', function(err, result, field) {
-						connection.release();
-						for (var index in result) {
-							var at_index_ = result[index].email.indexOf('@');
-							result[index].email = result[index].email.substr(0, 2)+'***@'+result[index].email.substr(at_index_+1, 2);
-						}
-						res.render('product_box', {product: product, productNo: productNo, comment: result});
+					connection.query('SELECT round(avg(star), 1) star from productComment where productNo='+productNo, function(err, result, field) {
+						if (err)
+							console.error(err);
+						var star = result[0];
+						connection.query('SELECT email, content FROM productComment c join user u on c.userNo = u.userNo where productNo='+productNo+' order by c.commentNo', function(err, result, field) {
+							connection.release();
+							for (var index in result) {
+								var at_index_ = result[index].email.indexOf('@');
+								result[index].email = result[index].email.substr(0, 2)+'***@'+result[index].email.substr(at_index_+1, 2);
+							}
+							res.render('product_box', {product: product, productNo: productNo, comment: result, star: star});
+						});
 					});
 				});
 			});
@@ -36,14 +43,21 @@ router.get('/', function(req, res, next) {
 				connection.query('use vasket');
 				connection.query('update product set view=view+1 where productNo='+productNo);
 				connection.query('select image, imageCount, content, view, category, productName, nation, size, material, description, waterproof, gender, price, code, (select count(*) from productComment where productNo=?) as count, (select count(*) from productLikeList where productNo=?) as `like`, (select count(*) from productClipList where productNo=?) as `clip` from product where productNo='+productNo, [productNo, productNo, productNo], function(err, result, field) {
+					if (err)
+						console.error(err);
 					var product = result[0];
-					connection.query('SELECT email, content FROM productComment c join user u on c.userNo = u.userNo where productNo='+productNo+' order by c.commentNo', function(err, result, field) {
-						connection.release();
-						for (var index in result) {
-							var at_index_ = result[index].email.indexOf('@');
-							result[index].email = result[index].email.substr(0, 2)+'***@'+result[index].email.substr(at_index_+1, 2);
-						}
-						res.render('product_box', {product: product, productNo: productNo, comment: result});
+					connection.query('SELECT round(avg(star), 1) star from productComment where productNo='+productNo, function(err, result, field) {
+						if (err)
+							console.error(err);
+						var star = result[0];
+						connection.query('SELECT email, content FROM productComment c join user u on c.userNo = u.userNo where productNo='+productNo+' order by c.commentNo', function(err, result, field) {
+							connection.release();
+							for (var index in result) {
+								var at_index_ = result[index].email.indexOf('@');
+								result[index].email = result[index].email.substr(0, 2)+'***@'+result[index].email.substr(at_index_+1, 2);
+							}
+							res.render('product_box', {product: product, productNo: productNo, comment: result, star: star});
+						});
 					});
 				});
 			});
