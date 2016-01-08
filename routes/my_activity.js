647d3cc11d2f4c5352c67ffba3e8f283563dd430
sangkyulee <sklee7753@gmail.com>
2016-01-08 17:55:16 +0900

    a

--- routes/my_activity.js
+++ routes/my_activity.js
@@ -16,13 +16,19 @@ router.get('/', function(req, res, next) {
 					console.error(err);
 				}
 				var community = result;
-				//connection.query('FILL SQL!!!', function(err, result, field) {
-				//	if (err) {
-				//		console.error(err);
-				//	}
-					connection.release();
-					res.render('my_activity', {community: community});
-				//});
+				connection.query('select c.contentNo, c.image, c.view, (select count(*) from contentClipList where contentNo=c.contentNo) clip, (select count(*) from contentComment where contentNo=c.contentNo) as `count`, (select count(*) from contentLikeList where contentNo=c.contentNo) as `like`, (select count(*) from contentLikeList where contentNo=c.contentNo and userNo=?) user_like, c.title from content c inner join contentClipList on c.contentNo = contentClipList.contentNo where userNo=? order by c.contentNo desc', [req.session.userNo, req.session.userNo], function(err, result, field) {
+					if (err) {
+						console.error(err);
+					}
+					var content = result;
+					connection.query('select p.productNo, p.image, p.view, (select count(*) from productClipList where productNo=p.productNo) clip, (select count(*) from productComment where productNo=p.productNo) as `count`, (select count(*) from productLikeList where productNo=p.productNo) as `like`, (select count(*) from productLikeList where productNo=p.productNo and userNo=?) user_like, p.title from product p inner join productClipList on p.productNo= productClipList.productNo where userNo=? order by p.productNo desc;', [req.session.userNo, req.session.userNo], function(err, result, field) {
+						if (err) {
+							console.error(err);
+						}	
+						connection.release();
+						res.render('my_activity', {community: community, content: content, product: result});
+					});
+				});
 			});
 		});
 
