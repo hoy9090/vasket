d11185211eb9d6c1d491b7f8a8581281bc8a3658
sangkyulee <sklee7753@gmail.com>
2016-01-08 16:20:06 +0900

    a

--- routes/product_box.js
+++ routes/product_box.js
@@ -26,12 +26,26 @@ router.get('/', function(req, res, next) {
 							console.error(err);
 						var star = result[0];
 						connection.query('SELECT commentNo, email, content, good, bad, star, (select isGood from productCommentGoodBad where userNo=? and commentNo = c.commentNo) isGood FROM productComment c join user u on c.userNo = u.userNo where productNo='+productNo+' order by c.commentNo desc', [req.session.userNo], function(err, result, field) {
-							connection.release();
-							for (var index in result) {
-								var at_index_ = result[index].email.indexOf('@');
-								result[index].email = result[index].email.substr(0, 2)+'***@'+result[index].email.substr(at_index_+1, 2);
+							if (err)
+								console.error(err);
+							var comment = result;
+							for (var index in comment) {
+								var at_index_ = comment[index].email.indexOf('@');
+								comment[index].email = comment[index].email.substr(0, 2)+'***@'+comment[index].email.substr(at_index_+1, 2);
 							}
-							res.render('product_box', {product: product, productNo: productNo, comment: result, star: star});
+							connection.query('select brandNo, brandName brand, brandImageName image from brand where brandNo=(select brandNo from product where productNo=?);', [productNo], function(err, result, field) {
+								if (err)
+									console.error(err);
+								var brandNo = result[0].brandNo;
+								var brand = result[0].brand;
+								var image = result[0].image;
+								connection.query('select count(*) as `like` from brandLikeList where brandNo='+brandNo+' and userNo='+req.session.userNo, function(err, result, field) {
+									if (err)
+										console.error(err);
+									connection.release();
+									res.render('product_box', {product: product, productNo: productNo, comment: comment, star: star, brand: brand, brandNo: brandNo, image: image});
+								});								
+							});
 						});
 					});
 				});
@@ -51,12 +65,19 @@ router.get('/', function(req, res, next) {
 							console.error(err);
 						var star = result[0];
 						connection.query('SELECT commentNo, email, content, good, bad, star FROM productComment c join user u on c.userNo = u.userNo where productNo='+productNo+' order by c.commentNo desc', function(err, result, field) {
-							connection.release();
-							for (var index in result) {
-								var at_index_ = result[index].email.indexOf('@');
-								result[index].email = result[index].email.substr(0, 2)+'***@'+result[index].email.substr(at_index_+1, 2);
+							if (err)
+								console.error(err);
+							var comment = result;
+							for (var index in comment) {
+								var at_index_ = comment[index].email.indexOf('@');
+								comment[index].email = comment[index].email.substr(0, 2)+'***@'+comment[index].email.substr(at_index_+1, 2);
 							}
-							res.render('product_box', {product: product, productNo: productNo, comment: result, star: star});
+							connection.query('select brandNo, brandName brand, brandImageName image from brand where brandNo=(select brandNo from product where productNo=?);', [productNo], function(err, result, field) {
+								if (err)
+									console.error(err);
+								connection.release();
+								res.render('product_box', {product: product, productNo: productNo, comment: comment, star: star, brand: result[0].brand, brandNo: result[0].brandNo, image: result[0].image});
+							});
 						});
 					});
 				});
